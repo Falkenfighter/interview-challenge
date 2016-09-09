d138728ac0faa8fa0e6b4c447eb057e103ae8195
Justin Graham <Justin.graham@dev9.com>
2016-09-09 08:29:55 -0700

    Added test classes to question and updated question module name

---
+++ yahtzee/question/src/test/java/com/graham/DiceTest.java
@@ -0,0 +1,12 @@
+package com.graham;
+
+import org.testng.annotations.Test;
+
+@Test
+public class DiceTest {
+
+    @Test
+    public void testRoll() {
+
+    }
+}
---
+++ yahtzee/question/src/test/java/com/graham/YahtzeeTest.java
@@ -0,0 +1,12 @@
+package com.graham;
+
+import org.testng.annotations.Test;
+
+@Test
+public class YahtzeeTest {
+
+    @Test
+    public void testMain() {
+
+    }
+}
